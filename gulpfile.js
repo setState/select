2504de22bc6116904daf0456e662b5c80c4fbd95
ineo6 <arklove@qq.com>
2016-04-29 18:14:53 +0800

    babelrc

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
-/**
- * Created by neo on 16/4/29.
- */
+var gulp = require('gulp'),
+  less = require('gulp-less');
+
+gulp.task('less', function () {
+  gulp.src('assets/index.less')
+    .pipe(less())
+    .pipe(gulp.dest('assets'));
+});
