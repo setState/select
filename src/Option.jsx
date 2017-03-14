58bad5177f6c52578291d1b0a5d0e668b3c2f6e3
neo <arklove@qq.com>
2017-03-14 17:22:51 +0800

    fix: 修复Option的Props语法错误

--- src/Option.jsx
+++ src/Option.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Option extends React.Component {
   static propTypes = {
-    value: React.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+    value: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
   };
 
   static isSelectOption = true;
